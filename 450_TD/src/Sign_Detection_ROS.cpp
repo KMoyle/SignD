20c8c5e9bf3421510c9512d2bf70a25f20a67f24
KMoyle <kylesmoyle@gmail.com>
2017-10-06 21:42:47 +1000

    added pose est of sign

--- 450_TD/src/Sign_Detection_ROS.cpp
+++ 450_TD/src/Sign_Detection_ROS.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "opencv2/core/core.hpp"
+#include <opencv2/calib3d.hpp>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include "sensor_msgs/Image.h"
@@ -13,9 +14,14 @@
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <cv.h>
+
+#include <tf2_ros/transform_broadcaster.h>
 #include "opencv2/opencv.hpp"
 #include <list>
 #include "std_msgs/Int64.h"
+
+#include <string>
+#include <iostream>
 
 #include "stdlib.h"
 #include "time.h"
@@ -45,14 +51,21 @@ class ImageConverter{
 
   	image_transport::ImageTransport it_;
   	image_transport::Subscriber image_sub_;
-  	image_transport::Publisher image_pub;
-  	ros::Publisher point_pub;
+  	image_transport::Publisher image_pub_;
+  	ros::Publisher pub_detect_;
 
   	bool got_camera_info_;
   	bool param_camera_rectified_;
 
   	sensor_msgs::CameraInfo camera_info_;
-  	cv::Mat camera_matrix_;
+	std::vector<cv::Point3d> model_points_;
+
+	bool param_show_marker_;
+	visualization_msgs::Marker ball_marker_;
+	
+	tf2_ros::TransformBroadcaster tfbr_;
+  	
+	cv::Mat camera_matrix_;
   	cv::Mat dist_coeffs_;
 
   	std::string topic_input_camera_info_;
@@ -66,11 +79,18 @@ class ImageConverter{
     		// Subscribe to input video feed and publish output video feed
     		image_sub_ = it_.subscribe(SUB_TOPIC, 1,&ImageConverter::imageCb, this);
 
-    		image_pub = it_.advertise(PUB_TOPIC, 1);
-    		point_pub = nh_.advertise<geometry_msgs::Point>("/object_location", 1);
+    		image_pub_ = it_.advertise(PUB_TOPIC, 1);
+    		pub_detect_ = nh_.advertise<geometry_msgs::PoseStamped>("/object_location", 1);
 
     		sub_camera_info_ = nh_.subscribe<sensor_msgs::CameraInfo> ("/cv_camera/camera_info",1, &ImageConverter::camera_info_cb, this);
     		//topic_input_camera_info_( "input_camera_info" );
+
+		double sign_rad = 5/ 2.0;
+		model_points_.push_back( cv::Point3d( 0.0, 0.0, 0.0 ) );	//Center
+		model_points_.push_back( cv::Point3d(-sign_rad, sign_rad, 0.0 ) );	//Top Left
+		model_points_.push_back( cv::Point3d( sign_rad, sign_rad, 0.0 ) );	//Top Right
+		model_points_.push_back( cv::Point3d(-sign_rad,-sign_rad, 0.0 ) );	//Bottom Left
+		model_points_.push_back( cv::Point3d( sign_rad,-sign_rad, 0.0 ) );	//Bottom Right
 
   	}
   ~ImageConverter() {}
@@ -104,14 +124,14 @@ class ImageConverter{
 	got_camera_info_ = true;
    }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  void imageCb(const sensor_msgs::ImageConstPtr& msg_in)
   {
     // Declare opencv mat pointer
     cv_bridge::CvImagePtr cv_ptr;
     // Try to convert recieved sensor_msgs/Image to opencv
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg_in, sensor_msgs::image_encodings::BGR8);
     }
     catch (cv_bridge::Exception& e)
     {
@@ -137,8 +157,63 @@ class ImageConverter{
     {
         Point center( blacksign[i].x + blacksign[i].width*0.5, blacksign[i].y + blacksign[i].height*0.5 );
         ellipse(cv_ptr->image, center, Size( blacksign[i].width*0.5, blacksign[i].height*0.5), 0, 0, 360, Scalar(255,0,255), 4, 8, 0 );
-	pointout.y = blacksign[0].y + blacksign[0].height*0.5;
-        pointout.x = blacksign[0].x + blacksign[0].width*0.5;
+
+	//Take circle estimate, and transform it to a 2D Square
+	std::vector<cv::Point2d> image_points;
+
+	//TODO: Should the be rounded?
+	double p_x =  blacksign[i].x ;	//Center of circle X
+	double p_y =  blacksign[i].y ;	//Center of circle Y
+	double p_d =  blacksign[i].width*0.5 ;	//Radius of circle
+
+	image_points.push_back( cv::Point2d( p_x, p_y ) );	//Center
+	image_points.push_back( cv::Point2d( p_x - p_d, p_y + p_d ) );	//Top Left
+	image_points.push_back( cv::Point2d( p_x + p_d, p_y + p_d ) );	//Top Right
+	image_points.push_back( cv::Point2d( p_x - p_d, p_y - p_d ) );	//Bottom Left
+	image_points.push_back( cv::Point2d( p_x + p_d, p_y - p_d ) );	//Bottom Right
+
+	cv::Mat rvec; // Rotation in axis-angle form
+	cv::Mat tvec;
+
+	// Solve for pose
+	cv::solvePnP( model_points_,
+				image_points,
+				camera_matrix_,
+				dist_coeffs_,
+				rvec,
+				tvec );
+	//Transmit the detection message
+	geometry_msgs::PoseStamped msg_out;
+
+	msg_out.header.frame_id = camera_info_.header.frame_id;
+	msg_out.header.stamp = msg_in->header.stamp;
+
+	msg_out.pose.position.x = tvec.at<double>(0,0);
+	msg_out.pose.position.y = tvec.at<double>(1,0);
+	msg_out.pose.position.z = tvec.at<double>(2,0);
+
+	//We don't have any orientation data about the ball, so don't even bother
+	msg_out.pose.orientation.w = 1.0;
+	msg_out.pose.orientation.x = 0.0;
+	msg_out.pose.orientation.y = 0.0;
+	msg_out.pose.orientation.z = 0.0;
+
+	geometry_msgs::TransformStamped tf_out;
+	tf_out.header = msg_out.header;
+	tf_out.child_frame_id = "Black Sign";
+	tf_out.transform.translation.x = msg_out.pose.position.x;
+	tf_out.transform.translation.y = msg_out.pose.position.y;
+	tf_out.transform.translation.z = msg_out.pose.position.z;
+	tf_out.transform.rotation.w = msg_out.pose.orientation.w;
+	tf_out.transform.rotation.x = msg_out.pose.orientation.x;
+	tf_out.transform.rotation.y = msg_out.pose.orientation.y;
+	tf_out.transform.rotation.z = msg_out.pose.orientation.z;
+
+	pub_detect_.publish( msg_out );
+	tfbr_.sendTransform( tf_out );
+			
+	//pointout.y = blacksign[0].y + blacksign[0].height*0.5;
+        //pointout.x = blacksign[0].x + blacksign[0].width*0.5;
     }
 /*
     red_sign_cascade.detectMultiScale( frame_gray, redsign, 1.1, 3, 0|CV_HAAR_SCALE_IMAGE, Size(100, 100),Size(275, 275));
@@ -165,8 +240,8 @@ class ImageConverter{
 */
 
 
-    image_pub.publish(cv_ptr->toImageMsg());
-    point_pub.publish(pointout);
+    image_pub_.publish(cv_ptr->toImageMsg());
+    //point_pub.publish(pointout);
     ROS_INFO("Image processed and sent");
 
   }
